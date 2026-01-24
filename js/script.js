3363559c28cca3bf1931e31f9fcb4bba96c19a60
Felipe ツ <fellipeds09@gmail.com>
2026-01-24 15:42:02 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function goToScreen(screenId) {
   }
 }
 
-const textos = ["Luna Lobo","Frontend Developer"];
+const textos = ["Felipe Silva","Frontend Developer"];
 const el = document.getElementById("digitando");
 
 let i=0,j=0,apagando=false;
